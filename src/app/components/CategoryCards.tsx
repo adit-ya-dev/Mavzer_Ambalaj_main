0a1a7aaa0e463bdf151319cabf54e55bcdd8a80a
adit-ya-dev <aditsingh8077@gmail.com>
2025-08-27 08:10:53 -0700

    updated feature X / fixed bug Y

--- src/app/components/CategoryCards.tsx
+++ src/app/components/CategoryCards.tsx
@@ -288,7 +288,7 @@ const CategoryCards: React.FC<CategoryCardsProps> = ({
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.3 }}
-              className="mt-8 text-center"
+              className="mt-25 text-center"
             >
               <Link href={categoryLink}>
                 <button
